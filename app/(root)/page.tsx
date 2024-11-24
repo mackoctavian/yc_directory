89bf2681bb8ba73c7f859fd519d96d308e45545e
Mack Octavian <maestrooctavian@gmail.com>
2024-11-25 01:02:03 +0300

    Added sanity types

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
 import { auth } from "@/auth";
 import SearchForm from "../../components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -8,21 +10,23 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = await client.fetch(STARTUPS_QUERY);
 
-  const posts = [
-    {
-      _createdAt: "yesterday",
-      views: 55,
-      author: { _id: 1, name: "Mack" },
-      _id: 1,
-      description: "This is a description",
-      image:
-        "https://images.unsplash.com/photo-1512820790803-83ca734da794?q=80&w=1498&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
+  // const posts = [
+  //   {
+  //     _createdAt: "yesterday",
+  //     views: 55,
+  //     author: { _id: 1, name: "Mack" },
+  //     _id: 1,
+  //     description: "This is a description",
+  //     image:
+  //       "https://images.unsplash.com/photo-1512820790803-83ca734da794?q=80&w=1498&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     category: "Robots",
+  //     title: "We Robots",
+  //   },
+  // ];
 
+  console.log(JSON.stringify(posts, null, 2));
   return (
     <>
       <section className="pink_container">
@@ -41,7 +45,9 @@ export default async function Home({
         </p>
         <ul className="mt-7 card-grid">
           {posts?.length > 0 ? (
-            posts.map((post) => <StartupCard key={post?._id} post={post} />)
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
           ) : (
             <p className="no-results">No startups found</p>
           )}
